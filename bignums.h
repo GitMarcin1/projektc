62a58e9c280dd0bf6d8e4b7eec36b38b05ee4675
Marcin <marcinplaza21@gmail.com>
2021-02-01 18:19:00 +0100

    mult1

--- bignums.h
+++ bignums.h
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+//co zwracaja poszzczegolne funkcje, odwolywanie sie do tablicy w funkcjach, wczytywanie czy dobrze, i wypisywanie, dzielneie
+
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 
@@ -15,12 +17,15 @@ char licz2[9900];
 int n,m;
 
 FILE *fp;
-fp=fopen ("liczba1.txt", "r");
+fp=fopen ("liczba1.txt", "r"); // liczba a
 
 FILE *fp2;
-fp2=fopen ("liczba2.txt", "r");
+fp2=fopen ("liczba2.txt", "r"); //liczba b
 
-void init(fp,fp2)
+FILE *fp3;
+fp3=fopen ("wynik.txt","w"); //liczba utworozna przy pomocy obliczen na a i b
+
+void init(*fp,*fp2)
 {
 
     int d1,d2;
@@ -126,7 +131,7 @@ void sub()
     if(n>m || a[n-1]>b[n-1])
     {
 
-    //odejmowanie kazdej komorki jak w odejmowaniu slupkowym
+        //odejmowanie kazdej komorki jak w odejmowaniu slupkowym
         for(int i=0; i<n; i++)
         {
             if(a[i]>b[i])
@@ -143,10 +148,10 @@ void sub()
 
         }
     }
-        else
+    else
 // jesli liczba ktora chcemy odjac jest wieksza od liczby od ktorej odejmujemy
 // zamieniamy je miejscami a wynik wypisujemy z minusem
-        {
+    {
 
         for(int i=0; i<m; i++)
         {
@@ -164,71 +169,191 @@ void sub()
 
         }
 
-        !printf -?
+        //  !printf -?
 
+    }
+
+
+}
+
+
+void mult()
+{
+
+    unsigned long long karatsuba(unsigned int a[],unsigned int b[])
+    {
+
+
+//maxs wieksza z dlugosci liczb
+        int rozmiar; // dlugosc podzielonej liczby na dwie czesci
+
+// int potega = 10^n;
+
+        if(maxs%2==0)
+        {
+            rozmiar=maxs/2;
+        }
+        else
+        {
+            rozmiar=(maxs/2) +1
         }
 
 
+        unsigned int liczbaa1[rozmiar]; // pierwsza czesc liczby a
+
+        unsigned int liczbaa2[rozmiar]; // druga czesc liczbya
+
+        for(int i=0; i<rozmiar; i++)
+        {
+
+            liczbaa1[i]=a[rozmiar + i];
+
+            liczbaa2[i]=a[i];
+        }
+
+        unsigned int liczbab1[rozmiar]; // pierwsza czesc liczby b
+
+        unsigned int liczbab2[rozmiar]; // druga czesc liczby b
+
+
+        for(int i=0; i<rozmiar; i++)
+        {
+
+            liczbab1[i]=b[rozmiar + i];
+
+            liczbab2[i]=b[i];
+        }
+
+        ac= karatsuba(liczbaa1[],liczbab1[]);
+        bd= karatsuba(liczbaa2[],liczbab2[]);
+        e= karatsuba(sum1(liczbaa1[],liczbaa2[]),sum1(liczbab1,liczbab2));
+        f=sum1(ac,bd);
+        g=sub1(e[],f[]);
+
+
+       // return w[]?
+
     }
 
+}
 
-    void mult()
+
+void div()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+void save(plik.txt)
+{
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+unsigned long long sum1(unsigned int a[],unsigned int b[])
+{
+
+    w[0]=(a[0]+b[0])%1000000000+nadmiar;
+
+
+    for (int i=1; i<maxs; i++)
     {
 
 
+        nadmiar=((a[i-1]+b[i-1])-(a[i-1]+b[i-1])%1000000000)/1000000000;
+
+        // nadmiar pierwsza cyfra sumy dwoch komorek tablic a i b jesli liczba przekracza 9 cyfr
 
 
+        w[i]=((a[i]+b[i])%1000000000)+nadmiar;
 
-
-
-
-
-
-
-
-
-
+        //suma dwoch komorek z nadmiarem z poprzedniej liczby
 
     }
 
 
-    void div()
+    return w[];
+}
+
+
+
+void sub()
+{
+
+//odejmowanie jesli liczba1 > liczba2
+    if(n>m || a[n-1]>b[n-1])
     {
 
+        //odejmowanie kazdej komorki jak w odejmowaniu slupkowym
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]>b[i])
+            {
+                w[i]=a[i]-b[i];
+            }
+            else // jesli komorka liczby1 jest mniejsza od komorki z liczby2 zapozyczamy z kolejnej
+                //komorki liczby1 jedynke i dodajemy jej wartosc do aktualnej komorki
+            {
+                a[i+1]=a[i+1]-1;
+                a[i]=a[i]+1000000000;
+                w[i]=a[i]-b[i];
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
     }
-
-
-    void save(plik.txt)
+    else
+// jesli liczba ktora chcemy odjac jest wieksza od liczby od ktorej odejmujemy
+// zamieniamy je miejscami a wynik wypisujemy z minusem
     {
 
+        for(int i=0; i<m; i++)
+        {
+            if(b[i]>a[i])
+            {
+                w[i]=b[i]-a[i];
+            }
+            else // jesli komorka liczby2 jest mniejsza od komorki z liczby1 zapozyczamy z kolejnej
+                //komorki liczby2 jedynke i dodajemy jej wartosc do aktualnej komorki
+            {
+                b[i+1]=b[i+1]-1;
+                b[i]=b[i]+1000000000;
+                w[i]=b[i]-a[i];
+            }
 
+        }
 
-
-
-
-
-
-
-
-
-
-
-
+        //  !printf -?
 
     }
 
+
+}
 
